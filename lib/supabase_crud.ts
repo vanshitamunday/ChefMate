33b330a27b234fba27835ecbd3dd61f63841c0f9
Dafisto <chrisreid2002@gmail.com>
2025-03-26 11:52:38 -0600

    adjusted table names

--- lib/supabase_crud.ts
+++ lib/supabase_crud.ts
@@ -1,6 +1,6 @@
 import supabase from "./supabase";
 
-const TABLE_NAME = "userTable";
+const TABLE_NAME = "user_table";
 
 export async function getUsers() {
     const { data, error } = await supabase.from(TABLE_NAME).select("*");
